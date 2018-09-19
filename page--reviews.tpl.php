ea77d26c50e8debaafc98335d4d9e9ca22ed22cc
palagpatski <palagpatski@gmail.com>
2018-09-19 14:21:52 +0800

    Test remove empty space on Reviews page

--- page--reviews.tpl.php
+++ page--reviews.tpl.php
@@ -123,26 +123,7 @@ echo art_get_sidebar($art_sidebar_left, $vnavigation_left, 'art-sidebar1'); ?>
                                 </div>
                                 <?php endif; ?>
                                 <?php else: ?>
-                                <div class="art-box art-post">
-                                    <div class="art-box-body art-post-body">
-                                        <div class="art-post-inner art-article">
-                                            <div class="art-postcontent">
-                                                <?php if (!empty($tabs)) { echo $tabs.'<div class="cleared"></div>'; }; ?>
-                                                <?php if (!empty($tabs2)) { echo $tabs2.'<div class="cleared"></div>'; } ?>
-                                                <?php if (isset($mission) && !empty($mission)) { echo '<div id="mission">'.$mission.'</div>'; }; ?>
-                                                <?php if (!empty($help)) { echo render($help); } ?>
-                                                <?php if (!empty($messages)) { echo $messages; } ?>
-                                                <?php if (isset($action_links) && !empty($action_links)): ?>
-                                                <ul class="action-links">
-                                                    <?php print render($action_links); ?>
-                                                </ul>
-                                                <?php endif; ?>
-                                            </div>
-                                            <div class="cleared"></div>
-                                        </div>
-                                        <div class="cleared"></div>
-                                    </div>
-                                </div>
+                                
                                 <?php echo art_content_replace(render($content)); ?>
                                 <?php endif; ?>
                                 <?php if (!empty($banner4)) { echo '<div id="banner4">'.render($banner4).'</div>'; } ?>
